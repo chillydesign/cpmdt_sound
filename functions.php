4422638515086240be97bc84d65e33827b7664cf
Charles Harvey <harvey.charles@gmail.com>
2020-03-18 11:32:59 +0100

    add back json api

--- functions.php
+++ functions.php
@@ -499,26 +499,26 @@ function disable_wp_emojicons()
 add_action('init', 'disable_wp_emojicons');
 
 
-function remove_json_api()
-{
+// function remove_json_api()
+// {
 
-    // Remove the REST API lines from the HTML Header
-    remove_action('wp_head', 'rest_output_link_wp_head', 10);
-    remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
-    // Remove the REST API endpoint.
-    remove_action('rest_api_init', 'wp_oembed_register_route');
-    // Turn off oEmbed auto discovery.
-    add_filter('embed_oembed_discover', '__return_false');
-    // Don't filter oEmbed results.
-    remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
-    // Remove oEmbed discovery links.
-    remove_action('wp_head', 'wp_oembed_add_discovery_links');
-    // Remove oEmbed-specific JavaScript from the front-end and back-end.
-    remove_action('wp_head', 'wp_oembed_add_host_js');
-    // Remove all embeds rewrite rules.
-  // add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
-}
-add_action('after_setup_theme', 'remove_json_api');
+//     // Remove the REST API lines from the HTML Header
+//     remove_action('wp_head', 'rest_output_link_wp_head', 10);
+//     remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
+//     // Remove the REST API endpoint.
+//     remove_action('rest_api_init', 'wp_oembed_register_route');
+//     // Turn off oEmbed auto discovery.
+//     add_filter('embed_oembed_discover', '__return_false');
+//     // Don't filter oEmbed results.
+//     remove_filter('oembed_dataparse', 'wp_filter_oembed_result', 10);
+//     // Remove oEmbed discovery links.
+//     remove_action('wp_head', 'wp_oembed_add_discovery_links');
+//     // Remove oEmbed-specific JavaScript from the front-end and back-end.
+//     remove_action('wp_head', 'wp_oembed_add_host_js');
+//     // Remove all embeds rewrite rules.
+//   // add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
+// }
+// add_action('after_setup_theme', 'remove_json_api');
 
 
 
